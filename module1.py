4507897264455a854727ac90562a4fdd61c214ad
Cherry Cappel <cherrycappel@gmail.com>
2022-02-06 22:17:43 -0600

    code from module 1

---
+++ module1.py
@@ -0,0 +1,26 @@
+# Module 1
+# Cherry Cappel
+
+#int
+num1 = 1
+print (num1)
+
+#float
+num2 = 1.1
+print (num2)
+
+#boolean
+status = True
+print (status)
+
+#string
+mystr="Hello class"
+print (mystr)
+
+#tuple
+mytup = (1, 2, 3, 4,)
+print (mytup)
+
+#dictionary
+mydic = {"key1":1, "key2": 2, "key3": 3}
+print (mydic)
